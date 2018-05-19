4a121152f6e3bdb893e46bc59062f825258a9e1c
Sevitas <sevcik.martin4@gmail.com>
2018-05-19 10:54:34 +0200

    ConjuredItemUpdater implementation

--- csharp/Updaters/ConjuredItemUpdater.cs
+++ csharp/Updaters/ConjuredItemUpdater.cs
@@ -8,9 +8,24 @@ namespace csharp.Updaters
 {
     public class ConjuredItemUpdater : ItemUpdater
     {
+        private const int normalDecreaseValue = 2;
+        private const int passedSellInDecreaseValue = 4;
+
         public override void UpdateItem(Item item)
+        {            
+            DecrementSellIn(item);
+
+            for (int i = 0; i < numberOfQualityDecrease(item); i++)
+            {
+                DecrementQuality(item);
+            }
+        }
+
+        private int numberOfQualityDecrease(Item item)
         {
-            
+            return item.SellIn > PassedSellInValue
+                ? Math.Min(item.Quality, normalDecreaseValue)
+                : Math.Min(item.Quality, passedSellInDecreaseValue);
         }
     }
 }
